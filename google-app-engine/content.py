491db72ea3600ddb0a73f1e232023129db75480c
Kristoffer Walker <kixxauth@gmail.com>
2010-08-29 08:42:58 -0400

    Serving 'pricing' page.
     On branch master
    	modified:   google-app-engine/content.py
    	modified:   google-app-engine/templates/all-pricing.css
    	modified:   google-app-engine/templates/pricing.html

--- google-app-engine/content.py
+++ google-app-engine/content.py
@@ -4,8 +4,6 @@ VERTICAL_DESCRIPTION = ('Kris Walker %s (for fun and profit) right here in %s an
 
 PRICING_DESCRIPTION = 'Upfront pricing for your website design, including free.'
 
-INTRO = 'I %s'
-
 LOCALES = {
       'poughkeepsie_and_hudson_valley': ['The Hudson Valley', 'Poughkeepsie']
     }
@@ -22,6 +20,23 @@ ACTIONS = {
         , ''
         ]
     }
+
+BASE_PRICING = {
+      'domain_reg'  : 107
+    , 'seo'         : 247
+    , 'setup'       : 227
+    , 'page'        : 247
+    , 'cms'         : 397
+    , 'cms_page'    : 297
+    , 'form'        : 797
+    , 'hourly'      : 57
+    , 'hosting'     : 12
+    , 'domain_svc' : 7
+    , 'seo_svc'    : 12
+    }
+
+PRICE_GROWING = 0.15
+PRICE_FULL = 0.25
 
 def vertical(locale, action):
   rv = {}
@@ -43,8 +58,47 @@ def vertical(locale, action):
   rv['item'] = actions[0]
   return rv
 
+def price_level(discount, known):
+  return int(known / (1 - discount))
+
 pricing = {
       'page_class': 'pricing'
     , 'description': PRICING_DESCRIPTION
+    , 'price_start_domain': BASE_PRICING['domain_reg']
+    , 'price_growing_domain': price_level(PRICE_GROWING, BASE_PRICING['domain_reg'])
+    , 'price_full_domain': price_level(PRICE_FULL, BASE_PRICING['domain_reg'])
+    , 'price_start_seo': BASE_PRICING['seo']
+    , 'price_growing_seo': price_level(PRICE_GROWING, BASE_PRICING['seo'])
+    , 'price_full_seo': price_level(PRICE_FULL, BASE_PRICING['seo'])
+    , 'price_start_hosting_setup': BASE_PRICING['setup']
+    , 'price_growing_hosting_setup': price_level(PRICE_GROWING, BASE_PRICING['setup'])
+    , 'price_full_hosting_setup': price_level(PRICE_FULL, BASE_PRICING['setup'])
+    , 'price_start_package': BASE_PRICING['page'] * 3
+    , 'price_growing_package': price_level(PRICE_GROWING, BASE_PRICING['page']) * 3
+    , 'price_full_package': price_level(PRICE_FULL, BASE_PRICING['page']) * 3
+    , 'price_start_page': BASE_PRICING['page']
+    , 'price_growing_page': price_level(PRICE_GROWING, BASE_PRICING['page'])
+    , 'price_full_page': price_level(PRICE_FULL, BASE_PRICING['page'])
+    , 'price_start_cms': BASE_PRICING['cms']
+    , 'price_growing_cms': price_level(PRICE_GROWING, BASE_PRICING['cms'])
+    , 'price_full_cms': price_level(PRICE_FULL, BASE_PRICING['cms'])
+    , 'price_start_cmspage': BASE_PRICING['cms_page']
+    , 'price_growing_cmspage': price_level(PRICE_GROWING, BASE_PRICING['cms_page'])
+    , 'price_full_cmspage': price_level(PRICE_FULL, BASE_PRICING['cms_page'])
+    , 'price_start_form': BASE_PRICING['form']
+    , 'price_growing_form': price_level(PRICE_GROWING, BASE_PRICING['form'])
+    , 'price_full_form': price_level(PRICE_FULL, BASE_PRICING['form'])
+    , 'price_start_hourly': BASE_PRICING['hourly']
+    , 'price_growing_hourly': price_level(PRICE_GROWING, BASE_PRICING['hourly'])
+    , 'price_full_hourly': price_level(PRICE_FULL, BASE_PRICING['hourly'])
+    , 'price_start_hosting': BASE_PRICING['hosting']
+    , 'price_growing_hosting': price_level(PRICE_GROWING, BASE_PRICING['hosting'])
+    , 'price_full_hosting': price_level(PRICE_FULL, BASE_PRICING['hosting'])
+    , 'price_start_domain_service': BASE_PRICING['domain_svc']
+    , 'price_growing_domain_service': price_level(PRICE_GROWING, BASE_PRICING['domain_svc'])
+    , 'price_full_domain_service': price_level(PRICE_FULL, BASE_PRICING['domain_svc'])
+    , 'price_start_seo_service': BASE_PRICING['seo_svc']
+    , 'price_growing_seo_service': price_level(PRICE_GROWING, BASE_PRICING['seo_svc'])
+    , 'price_full_seo_service': price_level(PRICE_FULL, BASE_PRICING['seo_svc'])
     }
 
